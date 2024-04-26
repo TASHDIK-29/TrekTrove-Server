63edf91af809f0115253123adf58a0ccd588a143
Tashdik <s2110477153@ru.ac.bd>
2024-04-26 15:20:14 +0600

    successfully feching single data from DB

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,7 +31,24 @@ async function run() {
         const database = client.db("TouristSpotDB");
         const touristSpotCollections = database.collection("touristSpots");
 
-        
+        app.get('/spots', async(req, res) =>{
+            const cursor = touristSpotCollections.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/spots', async(req, res) =>{
+            const spot = req.body;
+            const result = await touristSpotCollections.insertOne(spot);
+            res.send(result);
+        })
+
+        app.get('/spots/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query ={_id: new ObjectId(id)};
+            const result = await touristSpotCollections.findOne(query);
+            res.send(result);
+        })
 
 
 
